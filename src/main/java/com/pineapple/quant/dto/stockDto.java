a08ee9c0dfa9393a61b380e023fe7bb7ebad8afa
psm651 <psm651@naver.com>
2021-04-28 23:22:31 +0900

    jdbc 적용중

---
+++ src/main/java/com/pineapple/quant/dto/stockDto.java
@@ -0,0 +1,8 @@
+package com.pineapple.quant.dto;
+
+import lombok.Data;
+
+@Data
+public class stockDto {
+    private String stockName;
+}
---
+++ src/main/java/com/pineapple/quant/repository/StockRepository.java
@@ -0,0 +1,9 @@
+package com.pineapple.quant.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StockRepository {
+
+
+}
